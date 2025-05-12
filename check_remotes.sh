71658fc7570084fd2b54a637580824a377c554b2
Ya Udah <amzhakkeren225@gmail.com>
2025-05-12 20:13:01 +0700

    Update check_remotes.sh

--- check_remotes.sh
+++ check_remotes.sh
@@ -14,18 +14,15 @@ fi
 for remote in $remotes; do
     echo -e "\nChecking remote: $remote"
     
-    # Remove the trailing ':' from the remote name
-    #clean_remote=${remote%:}
-    
     # Check the remote with verbose listing
     echo "Running: rclone ls -v $remote"
     rclone ls -v "$remote"
     
     # Check the exit status of the command
     if [ $? -eq 0 ]; then
-        echo "Successfully checked $clean_remote"
+        echo "Successfully checked $remote"
     else
-        echo "Error checking $clean_remote" >&2
+        echo "Error checking $remote" >&2
     fi
 done
 
